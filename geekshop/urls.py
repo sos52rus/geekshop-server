4fa7f0cc67f056338dc4f14c24c9c69ec79b8048
sergey <sergey_volnukhin@mail.ru>
2021-05-26 13:27:31 +0300

    homework_lesson_1

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from products.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('products/', products, name='products'),
 ]
