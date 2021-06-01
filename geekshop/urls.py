3abce45affab360722d8091ee6fb75404a1c37ef
sergey <sergey_volnukhin@mail.ru>
2021-06-01 17:36:27 +0300

    task_5 setting up a project to work with media files

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from products.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
