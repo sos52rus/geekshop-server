82e6d71d8100d9969d9d0175b58b9e7c700c9024
sergey <sergey_volnukhin@mail.ru>
2021-06-04 11:24:54 +0300

    task_1 create users model

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True)
+    age = models.PositiveIntegerField(verbose_name='Возраст', null=True)
+    gender = [('М', 'Мужской'), ('Ж', 'Женский')]
+    sex = models.CharField(max_length=1, choices=gender)
+
+
+
