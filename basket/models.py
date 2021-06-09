d9d98fc404c90e34f28be91adc9ff2e839e08c78
sergey <sergey_volnukhin@mail.ru>
2021-06-09 11:50:41 +0300

    task_3 add basket app and models

---
+++ basket/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from products.models import Product
+from users.models import User
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
+    path('basket/', include('basket.urls', namespace='basket'))
 ]
 
 if settings.DEBUG:
